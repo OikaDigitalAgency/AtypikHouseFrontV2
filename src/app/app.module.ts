ba2378f450b6e2a66c400f321a7d868bd8775569
Quentin Vilcoque <q.vilcoque@gmail.com>
2021-08-11 14:56:18 +0200

    footer 1
    - ajout du html
    - ajout du css
    - test de l'affichage du footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
