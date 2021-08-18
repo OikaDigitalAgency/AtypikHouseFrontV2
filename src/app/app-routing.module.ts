404620a4b255403ccefbadecbadd90170a22f087
AbdelhakimLebiad <hakimlebiad@gmail.com>
2021-08-18 18:51:16 +0200

    Revert "Merge branch 'Dev' of https://github.com/OikaDigitalAgency/AtypikHouseFrontV2 into Dev"
    This reverts commit daae70d1d633356b2c0e1bd9e395c95d43f1de9a, reversing
    changes made to 891d2e42d3b6473451fa11198e293d41218d5a5b.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormContactComponent } from './components/form-contact/form-contact.component';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'contact', component: FormContactComponent}
+  { path: 'register', component: RegisterComponent }
 ];
 
 @NgModule({
