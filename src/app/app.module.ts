429b45e0627090cc91d3e1274f8ec8f702bde8e3
AbdelhakimLebiad <hakimlebiad@gmail.com>
2021-09-03 13:00:58 +0200

    front update login register home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { HomeComponent } from './home/home.component';
 import { PageContactComponent } from './page-contact/page-contact.component';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
