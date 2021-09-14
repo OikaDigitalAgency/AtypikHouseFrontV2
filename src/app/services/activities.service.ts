6ecee0bb3797835858391248fc627ac0db8a2c69
Hifumisama <m.khayat92@gmail.com>
2021-09-14 10:54:49 +0200

    [UPDATE] ménage

--- src/app/services/activities.service.ts
+++ src/app/services/activities.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IRegisterActivite,IActiviteEntity  } from '../models/activite';
+import { IRegisterActivite, IActiviteEntity } from '../models/activite';
 
 const AUTH_API = 'http://localhost:8000';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json', 
-    'Access-Control-Allow-Origin':'*', 
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
     'Authorization': sessionStorage.getItem('token')!
   })
 };
@@ -23,45 +24,45 @@ export class ActivitiesService {
   * Permet d'enregistrer une activité 
   * sur l'application
   */
- register(registerValues: IRegisterActivite): Observable<any> {
-   const body: IActiviteEntity = {
+  register(registerValues: IRegisterActivite): Observable<any> {
+    const body: IActiviteEntity = {
       title: registerValues.title,
       description: registerValues.description,
       locationlat: registerValues.locationlat,
       locationlng: registerValues.locationlng,
       idType: `\/api\/activities\/${registerValues.idType}`,
-   };
+    };
 
-   return this.http.post(`${AUTH_API}/api/activities_types`, body, httpOptions);
- }
+    return this.http.post(`${AUTH_API}/api/activities_types`, body, httpOptions);
+  }
 
-   /**
-  * Permet de modifier les activités 
-  * sur l'application
-  */
-    update(registerValues: IRegisterActivite): Observable<any> {
-     const body: IActiviteEntity = {
+  /**
+   * Permet de modifier les activités 
+   * sur l'application
+   */
+  update(registerValues: IRegisterActivite): Observable<any> {
+    const body: IActiviteEntity = {
       title: registerValues.title,
       description: registerValues.description,
       locationlat: registerValues.locationlat,
       locationlng: registerValues.locationlng,
       idType: `\/api\/activities\/${registerValues.idType}`,
-     };
- 
-     return this.http.put(`${AUTH_API}/api/activities_types`, body, httpOptions);
-   }
+    };
 
-   /**
-  * Permet de récupérer la liste des activités.
-  */
-    getAllComments(): Observable<any> {
-     return this.http.get(`${AUTH_API}/api/activities_types`, httpOptions);
-   }
+    return this.http.put(`${AUTH_API}/api/activities_types`, body, httpOptions);
+  }
 
-     /**
-  * Permet de supprimer la liste des activités.
-  */
+  /**
+   * Permet de récupérer la liste des activités.
+   */
+  getAllComments(): Observable<any> {
+    return this.http.get(`${AUTH_API}/api/activities_types`, httpOptions);
+  }
+
+  /**
+   * Permet de supprimer la liste des activités.
+   */
   deleteAllComments(): Observable<any> {
-   return this.http.delete(`${AUTH_API}/api/activities_types`, httpOptions);
- }
+    return this.http.delete(`${AUTH_API}/api/activities_types`, httpOptions);
+  }
 }
--- src/app/services/houses.service.ts
+++ src/app/services/houses.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IRegisterHouses,IHousesEntity  } from '../models/houses';
+import { IRegisterHouses, IHousesEntity } from '../models/houses';
 
 const AUTH_API = 'http://localhost:8000';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json', 
-    'Access-Control-Allow-Origin':'*', 
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
     'Authorization': sessionStorage.getItem('token')!
   })
 };
@@ -23,8 +24,8 @@ export class HousesService {
   * Permet d'enregistrer un nouveau house 
   * sur l'application
   */
- register(registerValues: IRegisterHouses): Observable<any> {
-   const body: IHousesEntity = {
+  register(registerValues: IRegisterHouses): Observable<any> {
+    const body: IHousesEntity = {
       title: registerValues.title,
       description: registerValues.description,
       address: registerValues.address,
@@ -40,17 +41,17 @@ export class HousesService {
       dateFin: registerValues.dateFin,
       idCategory: `\/api\/categories\/${registerValues.idCategory}`,
       idUser: `\/api\/users\/${registerValues.idUser}`,
-   };
+    };
 
-   return this.http.post(`${AUTH_API}/api/houses`, body, httpOptions);
- }
+    return this.http.post(`${AUTH_API}/api/houses`, body, httpOptions);
+  }
 
-   /**
-  * Permet de modifier les houses 
-  * sur l'application
-  */
-    update(registerValues: IRegisterHouses): Observable<any> {
-     const body: IHousesEntity = {
+  /**
+   * Permet de modifier les houses 
+   * sur l'application
+   */
+  update(registerValues: IRegisterHouses): Observable<any> {
+    const body: IHousesEntity = {
       title: registerValues.title,
       description: registerValues.description,
       address: registerValues.address,
@@ -66,22 +67,34 @@ export class HousesService {
       dateFin: registerValues.dateFin,
       idCategory: `\/api\/categories\/${registerValues.idCategory}`,
       idUser: `\/api\/users\/${registerValues.idUser}`,
-     };
- 
-     return this.http.put(`${AUTH_API}/api/houses`, body, httpOptions);
-   }
+    };
 
-   /**
-  * Permet de récupérer la liste des houses.
-  */
-    getAllComments(): Observable<any> {
-     return this.http.get(`${AUTH_API}/api/houses`, httpOptions);
-   }
+    return this.http.put(`${AUTH_API}/api/houses`, body, httpOptions);
+  }
 
-     /**
-  * Permet de supprimer la liste des houses.
-  */
-  deleteAllComments(): Observable<any> {
-   return this.http.delete(`${AUTH_API}/api/houses`, httpOptions);
- }
+  /**
+   * Permet de récupérer la liste des houses.
+   */
+  getAllHouses(): Observable<any> {
+    return this.http.get(`${AUTH_API}/api/houses`, httpOptions);
+  }
+
+  /**
+   * Permet de rechercher les appartements selon leur ville,
+   * leur disponibilité, et leur nombre de lits.
+   * @param city 
+   * @param dateFin 
+   * @param nbbeds 
+   * @returns 
+   */
+  searchHouses(city: string, dateFin: Date, nbbeds: number): Observable<any> {
+    return this.http.get(`${AUTH_API}/api/houses?city=${city}&dateFin[after]=${dateFin}&nbbeds[gte]=${nbbeds}&status=true`)
+  }
+
+  /**
+   * Permet de supprimer la liste des houses.
+   */
+  deleteAllHouses(): Observable<any> {
+    return this.http.delete(`${AUTH_API}/api/houses`, httpOptions);
+  }
 }
